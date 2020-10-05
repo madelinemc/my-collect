c5c2a3d59a40997d6f9ebd905c5e61d9793d4ac4
madelinemc <madeline.coons@gmail.com>
2020-10-04 21:24:56 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    collection = []
+    variable = 0
+    while variable < array.length
+        collection << yield(array[variable])
+        variable += 1
+    end
+    collection
+end
 
